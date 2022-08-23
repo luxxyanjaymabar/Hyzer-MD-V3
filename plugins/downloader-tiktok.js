c0b4cc5cf857cdf6c800feb11113c16f612df9b9
luxxyanjaymabar <110882963+luxxyanjaymabar@users.noreply.github.com>
2022-08-23 12:13:42 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,4 +1,13 @@
-const hxz = require("hxz-api")
+const hxz = require("{
+  "status": true,
+  "code": 200,
+  "creator": "Rey",
+  "result": {
+    "nowatermark": "https://ttdownloader.com/dl.php?v=YTo0OntzOjk6IndhdGVybWFyayI7YjowO3M6NzoidmlkZW9JZCI7czozMjoiMWJiNjZmZGYzOGE2YjIwZDQyNWJmOGE4N2JhNWU1ZjgiO3M6MzoidWlkIjtzOjMyOiIyY2EwYjk1MzgzODk5Y2E0MGYxNDQ3ZGJhYTczMjExNSI7czo0OiJ0aW1lIjtpOjE2NTc4MjM3NDY7fQ==",
+    "watermark": "https://ttdownloader.com/dl.php?v=YTo0OntzOjk6IndhdGVybWFyayI7YjoxO3M6NzoidmlkZW9JZCI7czozMjoiMWJiNjZmZGYzOGE2YjIwZDQyNWJmOGE4N2JhNWU1ZjgiO3M6MzoidWlkIjtzOjMyOiIyY2EwYjk1MzgzODk5Y2E0MGYxNDQ3ZGJhYTczMjExNSI7czo0OiJ0aW1lIjtpOjE2NTc4MjM3NDY7fQ==",
+    "audio": "https://ttdownloader.com/mp3.php?v=YTozOntzOjc6InZpZGVvSWQiO3M6MzI6IjFiYjY2ZmRmMzhhNmIyMGQ0MjViZjhhODdiYTVlNWY4IjtzOjM6InVpZCI7czozMjoiMmNhMGI5NTM4Mzg5OWNhNDBmMTQ0N2RiYWE3MzIxMTUiO3M6NDoidGltZSI7aToxNjU3ODIzNzQ2O30="
+  }
+}")
 let handler = async(m,{text, conn}) => {
 
 let p = await  hxz.ttdownloader(text)
